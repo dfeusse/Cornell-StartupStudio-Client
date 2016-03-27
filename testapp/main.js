aa1531714da8283f62dd680f611ea1ec35faac63
Daniel Feusse <dfeusse@gmail.com>
2016-03-26 22:29:44 -0400

    basic profile page

--- testapp/main.js
+++ testapp/main.js
@@ -13,8 +13,32 @@ console.log('main.js called');
 			name: 'Curl',
 			id: 2389017
 		}];
-	}
 
+		$scope.anna = {
+			name: "Anna Kournikova",
+			data: [{
+				day: "1/1",
+				exercise: "sauna",
+				minutes: 20
+			}, {
+				day: "1/2",
+				exercise: "squats",
+				minutes: 50
+			}],
+			img: "http://www.strangesports.com/images/content/133940.jpg" /*,
+			data = [{
+				day: "1/1/2016",
+				activity: "squat",
+				minutes: 50
+			}, {
+				day: "1/2/2016",
+				activity: "sauna",
+				minutes: 20
+			}] */
+		};
+
+		$scope.someValue = 'Hello';	
+	} // end of MainCtrl
 
 	angular
 		.module('app', [])

bf086c12da21641ac56a7580af3b6455dd766f63
Daniel Feusse <dfeusse@gmail.com>
2016-05-06 14:06:47 -0400

    added correct date for demo

--- apiapp/app.js
+++ apiapp/app.js
@@ -1,3 +1,11 @@
 console.log('app.js called');
 
 var app = angular.module("app", ['ngResource']);
+
+/*
+Need to:
+
+- do templates / views
+- finish the table in daypage
+- move onto visual? maybe
+*/
